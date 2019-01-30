11d57e834dd2b96ba236b520a4239c09681645cd
Jorge Flores <jflores2@paypal.com>
2019-01-30 15:50:57 -0600

    SC1: Adds WebLinkProviderTest

---
+++ src/test/java/com/sc/webcrawler/link/WebLinkProviderTest.java
@@ -0,0 +1,12 @@
+package com.sc.webcrawler.link;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WebLinkProviderTest {
+
+    @Test
+    void getLinks() {
+    }
+}
